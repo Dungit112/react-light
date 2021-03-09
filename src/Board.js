d0b67b98a2a665df20d62e5d2d497b27c05af514
Dungit112 <74139467+Dungit112@users.noreply.github.com>
2021-03-09 23:33:20 +0700

    Update Board.js
    update hook

--- src/Board.js
+++ src/Board.js
@@ -1,135 +1,93 @@
-import React, { Component } from "react";
-import Cell from "./Cell";
-import "./Board.css";
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
 
-class Board extends Component {
-  static defaultProps = {
-    nrows: 5,
-    ncols: 5,
-    chanceLightStartsOn: 0.25
-  };
-  constructor(props) {
-    super(props);
+import React, { useContext, useState, useRef, useEffect } from 'react'
+import Cell from './Cell'
+import './Board.css'
 
-    // TODO: set initial state
-    this.state = {
-      hasWon: false,
-      board: this.createBoard()
-    };
-  }
-
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-
-  createBoard() {
-    let board = [];
-    // TODO: create array-of-arrays of true/false values
-    for (let y = 0; y < this.props.nrows; y++) {
-      let row = [];
-      for (let x = 0; x < this.props.ncols; x++) {
-        row.push(Math.random() < this.props.chanceLightStartsOn);
-      }
-      board.push(row);
+const nrows = 5
+const ncols = 5
+const chanceLightStartsOn = 0.25
+const Board = () => {
+    const createBoard = () => {
+        let boardh = []
+        // TODO: create array-of-arrays of true/false values
+        for (let y = 0; y < nrows; y++) {
+            let row = []
+            for (let x = 0; x < ncols; x++) {
+                row.push(Math.random() < chanceLightStartsOn)
+            }
+            boardh.push(row)
+        }
+        return boardh
     }
-    return board;
-  }
-
-  /** handle changing a cell: update board & determine if winner */
-
-  flipCellsAround(coord) {
-    let { ncols, nrows } = this.props;
-    let board = this.state.board;
-    let [y, x] = coord.split("-").map(Number);
-
-    function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
-      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-        board[y][x] = !board[y][x];
-      }
+    const [hasWon, setHasWon] = useState(false)
+    const [board, setBoard] = useState(createBoard)
+   useEffect(()=>{
+     console.log("cscs");
+   })
+    const makeTable = () => {
+         let tblBoard = []
+        for (let y = 0; y < nrows; y++) {
+            let row = []
+            for (let x = 0; x < ncols; x++) {
+                let coord = `${y}-${x}`
+                row.push(<Cell key={coord} isLit={board[y][x]} flipCellsAroundMe={() => flipCellsAround(coord)} />)
+            }
+            tblBoard.push(<tr key={y}>{row}</tr>)
+        }
+        return tblBoard
+        // console.log(tblBoard);
+        // return (
+        //     <table className='Board'>
+        //         <tbody>{tblBoard}</tbody>
+        //     </table>
+        // )
     }
-    // TODO: flip this cell and the cells around it
-    flipCell(y, x); //Flip initial cell
-    flipCell(y, x - 1); //flip left
-    flipCell(y, x + 1); //flip right
-    flipCell(y - 1, x); //flip below
-    flipCell(y + 1, x); //flip above
+    const flipCellsAround = (coord) => {
+      let [y, x] = coord.split('-').map(Number)
 
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-    let hasWon = board.every(row => row.every(cell => !cell));
+      const flipCell = (y, x) => {
+          // if this coord is actually on board, flip it
 
-    this.setState({ board: board, hasWon: hasWon });
-  }
-
-  /** Render game board or winning message. */
-  makeTable() {
-    let tblBoard = [];
-    for (let y = 0; y < this.props.nrows; y++) {
-      let row = [];
-      for (let x = 0; x < this.props.ncols; x++) {
-        let coord = `${y}-${x}`;
-        row.push(
-          <Cell
-            key={coord}
-            isLit={this.state.board[y][x]}
-            flipCellsAroundMe={() => this.flipCellsAround(coord)}
-          />
-        );
+          if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+              board[y][x] = !board[y][x]
+          }
       }
-      tblBoard.push(<tr key={y}>{row}</tr>);
-    }
+      // TODO: flip this cell and the cells around it
+      flipCell(y, x) //Flip initial cell
+      flipCell(y, x - 1) //flip left
+      flipCell(y, x + 1) //flip right
+      flipCell(y - 1, x) //flip below
+      flipCell(y + 1, x) //flip above
+
+      // win when every cell is turned off
+      // TODO: determine is the game has been won
+      let hasWon = board.every((row) => row.every((cell) => !cell))
+      console.log("cscs");
+      setHasWon(hasWon)
+      setBoard(board)
+      // this.setState({ board: board, hasWon: hasWon });
+  }
     return (
-      <table className='Board'>
-        <tbody>{tblBoard}</tbody>
-      </table>
-    );
-  }
-  render() {
-    return (
-      <div>
-        {this.state.hasWon ? (
-          <div className='winner'>
-            <span className='neon-orange'>YOU</span>
-            <span className='neon-blue'>WIN!</span>
-          </div>
-        ) : (
-          <div>
-            <div className='Board-title'>
-              <div className='neon-orange'>Lights</div>
-              <div className='neon-blue'>Out</div>
-            </div>
-            {this.makeTable()}
-          </div>
-        )}
-      </div>
-    );
-  }
+        <div>
+            {hasWon ? (
+                <div className='winner'>
+                    <span className='neon-orange'>YOsU</span>
+                    <span className='neon-blue'>WIN!</span>
+                </div>
+            ) : (
+                <div>
+                    <div className='Board-title'>
+                        <div className='neon-orange'>Ligdvdhts</div>
+                        <div className='neon-blue'>Out</div>
+                    </div>
+                    {/* {makeTable()} */}
+                    <table className='Board'>
+                <tbody>{makeTable()}</tbody>
+            </table>
+                </div>
+           )}
+        </div>
+    )
 }
-
-export default Board;
+export default Board
